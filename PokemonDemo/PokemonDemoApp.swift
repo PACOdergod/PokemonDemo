189bfe1f9e9699e73c565ba302654d3c4f4461c3
PACOdergod <paco.33353@gmail.com>
2023-09-26 12:25:05 -0600

    Initial Commit

---
+++ PokemonDemo/PokemonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDemoApp.swift
+//  PokemonDemo
+//
+//  Created by FAMM on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
