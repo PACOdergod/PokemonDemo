90b24f84f4ca917b8368da328ee4bd9ba592252a
PACOdergod <paco.33353@gmail.com>
2023-09-26 18:32:20 -0600

    Hice los modelos para capturar la información que necesito de la pi

---
+++ PokemonDemo/Models/PokemonModel.swift
@@ -0,0 +1,26 @@
+//
+//  PokemonModel.swift
+//  PokemonDemo
+//
+//  Created by FAMM on 26/09/23.
+//
+
+import Foundation
+
+struct PokemonPage: Codable {
+    let count: Int
+    let next: String
+    let results: [PokemonModel]
+}
+
+struct PokemonModel: Codable, Equatable  {
+    let name: String
+    let url: String
+    
+    static var example = PokemonModel(name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/")
+}
+
+struct PokemonDetails: Codable {
+    let height: Int
+    let weight: Int
+}
